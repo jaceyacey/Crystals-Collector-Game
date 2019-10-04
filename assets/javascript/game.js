fb461bf2ccb2b82beff36a38dd2e92968e844008
Jace <cudnt_b_botherd@hotmail.com>
2019-10-04 14:25:14 +0800

    adjust total to 0 in new game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,26 @@
-//get random number
-let randomNum = Math.floor(Math.random() * 100) + 19;
-$("#random").html(randomNum);
-
-const generateCrystalNum = () => {
-  return Math.floor(Math.random() * 12) + 1;
-};
-
 let total = 0;
 let wins = 0;
 let losses = 0;
+let c1;
+let c2;
+let c3;
+let c4;
+let randomNum;
 
-//assign each crystal a number
-let c1 = generateCrystalNum();
-let c2 = generateCrystalNum();
-let c3 = generateCrystalNum();
-let c4 = generateCrystalNum();
+const newGame = () => {
+  //set total back to 0
+  total = 0;
+  $("#total").html(total);
+  //set random number
+  randomNum = Math.floor(Math.random() * 100) + 19;
+  $("#random").html(randomNum);
+  //assign each crystal a number
+  c1 = Math.floor(Math.random() * 12) + 1;
+  c2 = Math.floor(Math.random() * 12) + 1;
+  c3 = Math.floor(Math.random() * 12) + 1;
+  c4 = Math.floor(Math.random() * 12) + 1;
+};
 
-//add to total when crystal is clicked
 $("#1").click(function() {
   total += c1;
   $("#total").html(total);
@@ -24,11 +28,13 @@ $("#1").click(function() {
   if (randomNum === total) {
     wins++;
     $("#wins").html(wins);
+    newGame();
   }
   //loss
   if (total > randomNum) {
     losses++;
     $("#losses").html(losses);
+    newGame();
   }
 });
 
@@ -39,11 +45,13 @@ $("#2").click(function() {
   if (randomNum === total) {
     wins++;
     $("#wins").html(wins);
+    newGame();
   }
   //loss
   if (total > randomNum) {
     losses++;
     $("#losses").html(losses);
+    newGame();
   }
 });
 
@@ -54,11 +62,13 @@ $("#3").click(function() {
   if (randomNum === total) {
     wins++;
     $("#wins").html(wins);
+    newGame();
   }
   //loss
   if (total > randomNum) {
     losses++;
     $("#losses").html(losses);
+    newGame();
   }
 });
 
@@ -69,10 +79,14 @@ $("#4").click(function() {
   if (randomNum === total) {
     wins++;
     $("#wins").html(wins);
+    newGame();
   }
   //loss
   if (total > randomNum) {
     losses++;
     $("#losses").html(losses);
+    newGame();
   }
 });
+
+newGame();
